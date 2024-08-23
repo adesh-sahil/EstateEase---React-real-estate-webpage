ed0b1320925883d408abcb6b01c68aa297da96c0
adesh-sahil <97081855+adesh-sahil@users.noreply.github.com>
2024-08-24 00:46:42 +0530

    committed

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-    baseURL: "http://localhost:8800/api",
-    withCredentials: true,
+  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8800/api",
+  withCredentials: true,
 });
+
 
 export default apiRequest;
