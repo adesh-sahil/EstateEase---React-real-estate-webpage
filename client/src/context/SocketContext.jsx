6af887e259504a743b20bc023865d93ae77ddcdf
adesh-sahil <97081855+adesh-sahil@users.noreply.github.com>
2024-08-24 00:47:51 +0530

    committed

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -9,7 +9,7 @@ export const SocketContextProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    setSocket(io("http://localhost:4000"));
+    setSocket(io(process.env.REACT_APP_SOCKET_URL || "http://localhost:4000"));
   }, []);
 
   useEffect(() => {
