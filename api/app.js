c22c695c0a917dfba4cc98cee434aa50de35bf97
adesh-sahil <97081855+adesh-sahil@users.noreply.github.com>
2024-08-25 12:11:48 +0530

    committed

--- api/app.js
+++ api/app.js
@@ -25,6 +25,7 @@ app.use("/api/test", testRoute);
 
 
 
-app.listen (8800, () => {
-    console.log("server is running");
-})
+const PORT = process.env.PORT || 8800;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
