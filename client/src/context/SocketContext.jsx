bb7f774942b73e0fd0b2a398c02999bd5b6b3eca
Adesh <adeshkrmaharaj16@gmail.com>
2024-08-29 18:22:04 +0530

    committed

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -9,8 +9,8 @@ export const SocketContextProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    // setSocket(io("https://estateease-react-real-estate-webpage-1.onrender.com"));
-    setSocket(io("http://localhost:4000"));
+    setSocket(io("https://estateease-react-real-estate-webpage-1.onrender.com"));
+    // setSocket(io("http://localhost:4000"));
   }, []);
 
   useEffect(() => {
--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-    // baseURL: "https://estateease-react-real-estate-webpage.onrender.com/api",
-    baseURL: "http://localhost:8800/api",
+    baseURL: "https://estateease-react-real-estate-webpage.onrender.com/api",
+    // baseURL: "http://localhost:8800/api",
     withCredentials: true,
 });
 
