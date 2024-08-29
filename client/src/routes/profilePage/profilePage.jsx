14b0076df2defc812615a6ce9ef7cc02f9cb8ef3
Adesh <adeshkrmaharaj16@gmail.com>
2024-08-29 18:25:56 +0530

    committed

--- client/src/routes/profilePage/profilePage.jsx
+++ client/src/routes/profilePage/profilePage.jsx
@@ -181,29 +181,29 @@ function ProfilePage() {
               <button>Create New Post</button>
             </Link>
           </div>
-          <Suspense fallback={<p>Loading...</p>}>
+          {/* <Suspense fallback={<p>Loading...</p>}>
             <Await
               resolve={data.postResponse}
               errorElement={<p>Error loading posts!</p>}
             >
               {(postResponse) => <List posts={postResponse.data.userPosts} />}
             </Await>
-          </Suspense>
+          </Suspense> */}
           <div className="title">
             <h1>Saved List</h1>
           </div>
-          <Suspense fallback={<p>Loading...</p>}>
+          {/* <Suspense fallback={<p>Loading...</p>}>
             <Await
               resolve={data.postResponse}
               errorElement={<p>Error loading posts!</p>}
             >
               {(postResponse) => <List posts={postResponse.data.savedPosts} />}
             </Await>
-          </Suspense>
+          </Suspense> */}
         </div>
       </div>
       <div className="chatContainer">
-        <div className="wrapper">
+        {/* <div className="wrapper">
           <Suspense fallback={<p>Loading...</p>}>
             <Await
               resolve={data.chatResponse}
@@ -212,7 +212,7 @@ function ProfilePage() {
               {(chatResponse) => <Chat chats={chatResponse.data}/>}
             </Await>
           </Suspense>
-        </div>
+        </div> */}
       </div>
     </div>
   );
